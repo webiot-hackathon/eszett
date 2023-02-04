2f56ed1b9dd0ce7acda90dff7894d9bc68d66a85
Yoshihiko Kato aka discord4415 <raiden000discord@gmail.com>
2023-02-04 21:31:43 +0900

    add toku system

--- central_server/scripts.config.ts
+++ central_server/scripts.config.ts
@@ -4,8 +4,12 @@ import { DenonConfig } from "https://deno.land/x/denon@2.5.0/mod.ts";
 const config: DenonConfig = {
   scripts: {
     start: {
-      cmd: "deno run app.ts",
-      desc: "run my app.ts file",
+      cmd: "deno run server.ts",
+      desc: "run server system",
+    },
+    start_toku: {
+      cmd: "deno run server_toku.ts",
+      desc: "run toku system",
     },
   },
 };
