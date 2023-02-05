f4d1f044819733cfeb37f11bfb7e8ae18d444550
Yoshihiko Kato aka discord4415 <raiden000discord@gmail.com>
2023-02-05 13:18:44 +0900

    a

--- demoFront/js/map.js
+++ demoFront/js/map.js
@@ -7,5 +7,17 @@ SVG.on(document, 'DOMContentLoaded', function() {
     var car1 = draw.rect(50, 50).move(230,150).fill('red');
     var car2 = draw.rect(50,50).move(400,330).fill('black');
 
+    var request = new XMLHttpRequest();
+    var url  = "";
+    while (true) {
+        request.open('GET', url, true);
+    }
     
 });
+
+request.onload = function () {
+    // レスポンスが返ってきた時の処理
+    var data = this.response;
+    car1.move(data.car1.x, data.car1.y);
+    car1.move(data.car2.x, data.car2.y);
+}
